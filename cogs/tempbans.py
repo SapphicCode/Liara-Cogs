6e2160a943ebacb4efb3c286212839deb47a83cb
Pandentia <hi@pandentia.cf>
2017-04-08 23:51:07 +0200

    Add a cog for temporary bans

---
+++ cogs/tempbans.py
@@ -0,0 +1,73 @@
+from cogs.utils import checks
+from cogs.utils import dataIO
+from discord.ext import commands
+import discord
+import dateparser
+import datetime
+import asyncio
+
+
+class TempBans:
+    def __init__(self, liara):
+        self.loaded = True
+        self.liara = liara
+        self.banlist = dataIO.load('pandentia.tempbans')
+        if 'bans' not in self.banlist:
+            self.banlist['bans'] = []
+        self.task = self.liara.loop.create_task(self.unban_task())
+
+    def __unload(self):
+        self.loaded = False
+
+    async def get_user(self, uid):
+        return self.liara.get_user(uid) or await self.liara.get_user_info(uid)
+
+    async def unban_task(self):
+        while self.loaded:
+            for ban in list(self.banlist['bans']):
+                if ban['time'] > datetime.datetime.utcnow():
+                    continue
+                guild = self.liara.get_guild(ban['guild'])
+                if guild is None:
+                    continue
+                try:
+                    self.liara.dispatch('pandentia_tempbans_unban', ban['member'])
+                    await guild.unban(await self.get_user(ban['member']))
+                except discord.DiscordException:
+                    pass
+                finally:
+                    self.banlist['bans'].remove(ban)
+                await asyncio.sleep(0.05)
+            await asyncio.sleep(5)
+
+    @commands.command(no_pm=True)
+    @checks.mod_or_permissions(ban_members=True)
+    async def tempban(self, ctx, member: discord.Member, *, time: str):
+        """Temporarily bans a member."""
+        parsed = dateparser.parse('in '+time, settings={'TIMEZONE': 'UTC', 'PREFER_DATES_FROM': 'future'})
+        if parsed is None:
+            await ctx.send('That date couldn\'t be parsed. Try again.')
+            return
+        delta = (parsed - datetime.datetime.utcnow()).total_seconds()
+        if delta < 0:
+            await ctx.send('Sorry, I can\'t time travel.')
+            return
+        if delta < 120:  # 2 minutes
+            await ctx.send('Sorry, that duration is a bit too short.')
+            return
+        if delta > 2628000:  # 1 month
+            await ctx.send('Sorry, that duration is a bit too long.')
+            return
+
+        try:
+            await member.ban()
+            self.banlist['bans'].append({'time': parsed, 'member': member.id, 'guild': ctx.guild.id})
+        except discord.Forbidden:
+            await ctx.send('I don\'t have permission to ban that person here.')
+            return
+
+        await ctx.send('Banned. That member will be unbanned on {}.'.format(parsed))
+
+
+def setup(liara):
+    liara.add_cog(TempBans(liara))
